62f10c7d2646bf8e6e44bec1ddd3ec8d2548e527
Shofi <github@123xyz.addy.io>
2026-08-24 22:58:43 +0100

    Fix NSG rule: destination_port_range takes a single string, not a list

--- 01_basics/main.tf
+++ 01_basics/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_network_security_rule" "weballow" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_ranges     = ["80", "443"]
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.vnetrg.name
